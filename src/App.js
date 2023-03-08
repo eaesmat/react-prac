21de0935b86363601d2b57f15cc74712b8b418c7
eaesmat <ea.ahmadzai2020@gmail.com>
2023-03-08 15:23:42 +0430

    Make the components more readable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Expenses from "./Components/Expenses";
 
 function App() {
-return <Expenses></Expenses>
+  const items = [
+    { id: "i1", name: "Car", date: new Date(2020, 1, 13), amount: 200 },
+    { id: "i2", name: "PC", date: new Date(2021, 11, 23), amount: 400 },
+    { id: "i3", name: "Pen", date: new Date(2023, 12, 12), amount: 600 },
+    { id: "i4", name: "Book", date: new Date(2022, 5, 17), amount: 900 },
+  ];
+return <Expenses items = {items}></Expenses>
 }
 
 export default App;
--- src/Components/Expenses.js
+++ src/Components/Expenses.js
@@ -1,39 +1,34 @@
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 
-const Expenses = () => {
-  const items = [
-    { id: "i1", name: "Car", date: new Date(2020, 1, 13), amount: 200 },
-    { id: "i2", name: "PC", date: new Date(2021, 11, 23), amount: 400 },
-    { id: "i3", name: "Pen", date: new Date(2023, 12, 12), amount: 600 },
-    { id: "i4", name: "Book", date: new Date(2022, 5, 17), amount: 900 },
-  ];
+const Expenses = (props) => {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
-        name={items[0].name}
-        date={items[0].date}
-        amount={items[0].amount}
+        name={props.items[0].name}
+        date={props.items[0].date}
+        amount={props.items[0].amount}
       ></ExpenseItem>
 
       <ExpenseItem
-        name={items[1].name}
-        date={items[1].date}
-        amount={items[1].amount}
+        name={props.items[1].name}
+        date={props.items[1].date}
+        amount={props.items[1].amount}
       ></ExpenseItem>
 
       <ExpenseItem
-        name={items[2].name}
-        date={items[2].date}
-        amount={items[2].amount}
+        name={props.items[2].name}
+        date={props.items[2].date}
+        amount={props.items[2].amount}
       ></ExpenseItem>
 
       <ExpenseItem
-        name={items[3].name}
-        date={items[3].date}
-        amount={items[3].amount}
+        name={props.items[3].name}
+        date={props.items[3].date}
+        amount={props.items[3].amount}
       ></ExpenseItem>
-    </div>
+    </Card>
   );
 };
 
