f0030f6ef8e28c3231f04c9f9122e2655e24e080
eaesmat <ea.ahmadzai2020@gmail.com>
2023-03-12 14:51:23 +0430

    Add map to array for avoiding hard code

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,33 +1,17 @@
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
-import './Expenses.css';
+import "./Expenses.css";
 
 const Expenses = (props) => {
   return (
     <Card className="expenses">
-      <ExpenseItem
-        name={props.items[0].name}
-        date={props.items[0].date}
-        amount={props.items[0].amount}
-      ></ExpenseItem>
-
-      <ExpenseItem
-        name={props.items[1].name}
-        date={props.items[1].date}
-        amount={props.items[1].amount}
-      ></ExpenseItem>
-
-      <ExpenseItem
-        name={props.items[2].name}
-        date={props.items[2].date}
-        amount={props.items[2].amount}
-      ></ExpenseItem>
-
-      <ExpenseItem
-        name={props.items[3].name}
-        date={props.items[3].date}
-        amount={props.items[3].amount}
-      ></ExpenseItem>
+      {props.items.map((expense) => (
+        <ExpenseItem
+          name={expense.name}
+          date={expense.date}
+          amount={expense.amount}
+        ></ExpenseItem>
+      ))}
     </Card>
   );
 };
